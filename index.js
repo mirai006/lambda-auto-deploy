9e931a59176c54e2207914555492e2b086791a22
mirai006 <60591872+mirai006@users.noreply.github.com>
2025-05-28 15:39:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: "Updated Lambda! Now with new logic!"
+        body: "Updated Lambda! Now with new logic v2 !"
     };
 };
